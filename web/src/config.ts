34d76b50442b145578b40cc2956d47ae56244fef
Kevin Tjiam <kevin@kevincharm.com>
2021-03-04 02:55:36 +0100

    update API_ENDPOINT fallback

--- web/src/config.ts
+++ web/src/config.ts
@@ -1,9 +1,10 @@
 const isProduction = process.env.NODE_ENV === 'production'
 
 const config = {
-    apiEndpoint: isProduction
-        ? process.env.API_ENDPOINT || 'https://poll.indexed.finance'
-        : 'http://127.0.0.1:3000',
+    apiEndpoint:
+        isProduction ||
+        (process.env.API_ENDPOINT && 'https://poll.indexed.finance') ||
+        'http://127.0.0.1:3000',
 }
 
 export default config
