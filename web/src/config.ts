5d3b97315af67ba1dce0d474c5e39b2ca735abbb
Kevin Tjiam <kevin@kevincharm.com>
2021-03-04 02:04:52 +0100

    compile server to es2019 for node v12, add API_ENDPOINT env

--- web/src/config.ts
+++ web/src/config.ts
@@ -1,7 +1,9 @@
 const isProduction = process.env.NODE_ENV === 'production'
 
 const config = {
-    apiEndpoint: isProduction ? 'https://ndx-poll.spicyme.me' : 'http://127.0.0.1:3000',
+    apiEndpoint: isProduction
+        ? process.env.API_ENDPOINT || 'https://poll.indexed.finance'
+        : 'http://127.0.0.1:3000',
 }
 
 export default config
