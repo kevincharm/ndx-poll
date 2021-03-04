338fced672c3c365b21f5fdfb066d371b39c09cb
Kevin Tjiam <kevin@kevincharm.com>
2021-03-04 12:00:20 +0100

    use API_ENDPOINT instead of default endpoint

--- web/src/config.ts
+++ web/src/config.ts
@@ -1,10 +1,5 @@
-const isProduction = process.env.NODE_ENV === 'production'
-
 const config = {
-    apiEndpoint:
-        isProduction ||
-        (process.env.API_ENDPOINT && 'https://poll.indexed.finance') ||
-        'http://127.0.0.1:3000',
+    apiEndpoint: process.env.API_ENDPOINT || 'http://127.0.0.1:3000',
 }
 
 export default config
